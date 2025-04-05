2b6cba5c47197c571a508109d58d329049cb1e56
Blake Hodges <tbhodges@gmail.com>
2025-04-04 23:51:28 -0400

    chore(config) :remove default next config and set up proxy for /api

--- server/index.js
+++ server/index.js
@@ -1,38 +1,49 @@
 const path = require('path')
 const express = require('express');
-const next = require('next');
 const connectDB = require('./database')
 const handleServerError = require('./helpers/handleServerError')
 const { list, create, userByID, remove, update } = require('./controllers/user.controller')
 
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-const server = express();
-
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }))
-
-server.get('/api/users', list)
-
-server.post('/api/users', create)
-
-server.get('/api/users/:userId', userByID)
-
-server.put('/api/users/:userId', update)
-
-server.delete('/api/users/:userId', remove)
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
 
-app.prepare().then(() => {
-    server.all('*', (req, res) => handle(req, res));
+const app = express()
 
-    server.listen(3000, (err) => {
-        if (err) throw err;
-        console.log('Server ready at http://localhost:3000');
-    });
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/api/users', list)
+
+app.post('/api/users', create)
+
+app.get('/api/users/:userId', userByID)
+
+app.put('/api/users/:userId', update)
+
+app.delete('/api/users/:userId', remove)
+
+app.listen(3022, (err) => {
+    if (err) {
+        throw err
+    }
+    console.log('Server ready at http://localhost:3022');
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+// app.prepare().then(() => {
+//     server.all('*', (req, res) => handle(req, res));
+
+//     server.listen(3000, (err) => {
+//         if (err) throw err;
+//         console.log('Server ready at http://localhost:3000');
+//     });
+// });
 
 
